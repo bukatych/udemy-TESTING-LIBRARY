74b4cd65728e2a280e8d3f64d28a1cfaa06dee83
Illia Bukatych <i.bukatych@combocurve.com>
2023-06-30 00:05:02 +0300

    Setup tests to run properly

--- client/src/pages/entry/tests/totalUpdates.test.jsx
+++ client/src/pages/entry/tests/totalUpdates.test.jsx
@@ -1,10 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Options from '../Options';
+import { OrderDetailsProvider } from '../../../contexts/OrderDetails';
+import '@testing-library/jest-dom/extend-expect';
 
 test('update scoop subtotal when scoops change', async () => {
     const user = userEvent.setup();
-    render(<Options optionType="scoops" />);
+    render(<Options optionType="scoops" />, { wrapper: OrderDetailsProvider });
 
     // make sure total starts out $0.00
     const scoopsSubtotal = screen.getByText('Scoops total: $', {
@@ -18,16 +20,20 @@ test('update scoop subtotal when scoops change', async () => {
         name: 'Vanilla',
     });
 
-    await user.clear(vanillaInput);
-    await user.type(vanillaInput, '1');
-    expect(scoopsSubtotal).toHaveTextContent('2.00');
+    await waitFor(async () => {
+        await user.clear(vanillaInput);
+        await user.type(vanillaInput, '1');
+        expect(scoopsSubtotal).toHaveTextContent('2.00');
+    });
 
     // update chocolate scoops to 2 and check the subtotal
     const chocolateInput = await screen.findByRole('spinbutton', {
         name: 'Chocolate',
     });
 
-    await user.clear(chocolateInput);
-    await user.type(chocolateInput, '2');
-    expect(scoopsSubtotal).toHaveTextContent('6.00');
+    await waitFor(async () => {
+        await user.clear(chocolateInput);
+        await user.type(chocolateInput, '1');
+        expect(scoopsSubtotal).toHaveTextContent('4.00');
+    });
 });
