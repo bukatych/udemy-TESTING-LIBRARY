e7b5e228e67a9d87ece752d04b3b878e875f1a41
Illia Bukatych <i.bukatych@combocurve.com>
2023-06-30 13:51:47 +0300

    Write tests for toppings, cleaning

--- client/src/pages/entry/tests/totalUpdates.test.jsx
+++ client/src/pages/entry/tests/totalUpdates.test.jsx
@@ -5,7 +5,6 @@ import {
 } from '../../../test-utils/testing-library-utils';
 import userEvent from '@testing-library/user-event';
 import Options from '../Options';
-import { OrderDetailsProvider } from '../../../contexts/OrderDetails';
 import '@testing-library/jest-dom/extend-expect';
 
 test('update scoop subtotal when scoops change', async () => {
@@ -39,5 +38,53 @@ test('update scoop subtotal when scoops change', async () => {
         await user.clear(chocolateInput);
         await user.type(chocolateInput, '1');
         expect(scoopsSubtotal).toHaveTextContent('4.00');
+    });
+});
+
+test('update toppings subtotal when topping chosen', async () => {
+    const user = userEvent.setup();
+    render(<Options optionType="toppings" />);
+
+    // make sure total starts out $0.00
+    const toppingsSubtotal = screen.getByText('Toppings total: $', {
+        exact: false,
+    });
+
+    expect(toppingsSubtotal).toHaveTextContent('0.00');
+
+    const cherryCheckbox = await screen.findByRole('checkbox', {
+        name: 'Cherries',
+    });
+
+    await waitFor(async () => {
+        await user.click(cherryCheckbox);
+        expect(cherryCheckbox).toBeChecked();
+        expect(toppingsSubtotal).toHaveTextContent('1.50');
+    });
+
+    const mAndMCheckbox = await screen.findByRole('checkbox', {
+        name: 'M&Ms',
+    });
+
+    await waitFor(async () => {
+        await user.click(mAndMCheckbox);
+        expect(mAndMCheckbox).toBeChecked();
+        expect(toppingsSubtotal).toHaveTextContent('3.00');
+    });
+
+    const hotFudgeCheckbox = await screen.findByRole('checkbox', {
+        name: 'Hot fudge',
+    });
+
+    await waitFor(async () => {
+        await user.click(hotFudgeCheckbox);
+        expect(hotFudgeCheckbox).toBeChecked();
+        expect(toppingsSubtotal).toHaveTextContent('4.50');
+    });
+
+    await waitFor(async () => {
+        await user.click(cherryCheckbox);
+        expect(cherryCheckbox).not.toBeChecked();
+        expect(toppingsSubtotal).toHaveTextContent('3.00');
     });
 });
